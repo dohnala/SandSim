5bc3c60fe3951f501c9d2ef39325ffadb0b7161c
dohnal <dohnal.adam@gmail.com>
2021-02-20 10:47:26 +0100

    Updates

---
+++ precache-manifest.6efb29f2526ff76d5245ecc6c6316d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb39c36f5f9de0152147",
+    "url": "/SandSim/1.ee63f5f1aaffe1537243.js"
+  },
+  {
+    "revision": "4c8afe714c8101792360",
+    "url": "/SandSim/2.54fc1de06e0122574e2c.js"
+  },
+  {
+    "revision": "bb39c36f5f9de0152147",
+    "url": "/SandSim/58f9c0adbd71662491f9.module.wasm"
+  },
+  {
+    "revision": "145d2450e2faf6b3c855ba0863de063d",
+    "url": "/SandSim/index.html"
+  },
+  {
+    "revision": "de336c9fc552e2ddaada",
+    "url": "/SandSim/main.806a52516015daeea232.js"
+  },
+  {
+    "revision": "19079956d42c00d42ccf8e57d5bffdbc",
+    "url": "/SandSim/styles.css"
+  }
+]);
