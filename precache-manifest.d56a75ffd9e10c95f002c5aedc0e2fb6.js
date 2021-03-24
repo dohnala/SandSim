e50de5d548b1d03f8b9064d26c829d15e119c8a5
dohnal <dohnal.adam@gmail.com>
2021-03-24 12:58:27 +0100

    Updates

---
+++ precache-manifest.d56a75ffd9e10c95f002c5aedc0e2fb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96a0f0505d2647dc7fd",
+    "url": "/SandSim/1.16e6163ed2f8c702dc53.js"
+  },
+  {
+    "revision": "775ef81181aab3e72265",
+    "url": "/SandSim/2.e574836f6e47719a9a7a.js"
+  },
+  {
+    "revision": "b96a0f0505d2647dc7fd",
+    "url": "/SandSim/92eaf6814b782a6d675c.module.wasm"
+  },
+  {
+    "revision": "30fd031b1c7d339ee5c3204dc8d0b79c",
+    "url": "/SandSim/assets/bootstrap-icons.woff"
+  },
+  {
+    "revision": "316400c09c0560b3812e58b3124bbf15",
+    "url": "/SandSim/assets/bootstrap-icons.woff2"
+  },
+  {
+    "revision": "dee2417cd42c63d2929c02bc4d6e8903",
+    "url": "/SandSim/index.html"
+  },
+  {
+    "revision": "ddaac69e1cc71c1e39c7",
+    "url": "/SandSim/main.eaccee2aa547c65fbd61.js"
+  },
+  {
+    "revision": "46c12a29da36d2e77e7c35803cdc85ce",
+    "url": "/SandSim/styles.css"
+  }
+]);
